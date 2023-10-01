bf3e926ac554ce298267b7ac4afd3ef07a3b62a0
sunder3422 <36584153+sunder3422@users.noreply.github.com>
2023-10-01 13:43:43 +0530

    initialUpdate
    printed 3 sop lines

---
+++ Firstapp.java
@@ -0,0 +1,9 @@
+class Firstapp{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello this is first update");
+         System.out.println("Hello this is Second update");
+         System.out.println("Hello this is Third update update");
+
+    }
+}
